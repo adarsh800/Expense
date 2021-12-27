3f82afb2e81c6f0b69c772deba4bcf1461ac8040
Adarsh Shukla <78147487+adarsh800@users.noreply.github.com>
2021-12-27 16:33:10 +0530

    Update settings.py

--- expenseweb/settings.py
+++ expenseweb/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
